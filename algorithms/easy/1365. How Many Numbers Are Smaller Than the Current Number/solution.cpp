8920e642a90182e820ea376cf2360a94973acec6
a3amat1K <81564718+a3a256@users.noreply.github.com>
2023-07-17 16:13:10 +0100

    solved 1365

---
+++ algorithms/easy/1365. How Many Numbers Are Smaller Than the Current Number/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        int i, j, count;
+        vector<int> res;
+        for(i=0; i<nums.size(); i++){
+            count = 0;
+            for(j=0; j<nums.size(); j++){
+                if(nums[j]<nums[i]){
+                    count ++;
+                }
+            }
+            res.push_back(count);
+        }
+        return res;
+    }
+};

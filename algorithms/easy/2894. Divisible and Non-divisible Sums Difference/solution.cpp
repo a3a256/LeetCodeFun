96f7c5e1d08b32042ca924e8dcbed87c8f88a1b7
a3amat1K <81564718+a3a256@users.noreply.github.com>
2025-05-27 09:48:35 +0100

    solved 2894

---
+++ algorithms/easy/2894. Divisible and Non-divisible Sums Difference/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int differenceOfSums(int n, int m) {
+        int i, num1, num2;
+        num1 = 0;
+        num2 = 0;
+        for(i=1; i<=n; i++){
+            if(i%m != 0){
+                num1 += i;
+            }else{
+                num2 += i;
+            }
+        }
+        return num1 - num2;
+    }
+};

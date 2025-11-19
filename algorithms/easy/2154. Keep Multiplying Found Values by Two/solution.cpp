c5560381fd770945e1de367f00ca074eb3c0896c
a3amat1K <81564718+a3a256@users.noreply.github.com>
2025-11-19 22:36:37 +0000

    solved 2154

---
+++ algorithms/easy/2154. Keep Multiplying Found Values by Two/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int original) {
+        map<int, int> mp;
+        for(int i: nums){
+            mp[i]++;
+        }
+        while(mp[original] != 0){
+            original *= 2;
+        }
+        return original;
+    }
+};
